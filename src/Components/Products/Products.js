43d534d2dc5648ed8283c1afa2fb48ae2e679c48
sumon092 <m.sumon@outlook.com>
2022-03-23 02:59:58 +0600

    shopping cart product added to shop

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -4,13 +4,13 @@ import './Products.css'
 const Products = (props) => {
     // console.log(props);
     const { img, name, seller, ratings, price } = props.product;
-    console.log(props);
+    // console.log(props);
     return (
         <div className='products'>
             <img src={img} alt="" />
             <div className="product-info">
                 <p className="product-name">{name}</p>
-                <p>Price : {price}</p>
+                <p>Price :$ {price}</p>
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Rating: {ratings} stars</small></p>
             </div>
