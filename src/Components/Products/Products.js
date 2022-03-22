394d10eb00391dc6451d840daf8bf9588f8830d1
sumon092 <m.sumon@outlook.com>
2022-03-23 01:12:17 +0600

    products added

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Products.css'
+
+const Products = (props) => {
+    console.log(props);
+    const { img, name, seller, ratings, price } = props.product;
+    return (
+        <div className='products'>
+            <img src={img} alt="" />
+            <div className="product-info">
+                <p className="product-name">{name}</p>
+                <p>Price : {price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings} stars</small></p>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
