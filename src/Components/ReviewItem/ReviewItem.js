59ee34c5f627c299d540cd835707381b82857533
sumon092 <m.sumon@outlook.com>
2022-04-01 01:53:48 +0600

    order detail componet is working

--- src/Components/ReviewItem/ReviewItem.js
+++ src/Components/ReviewItem/ReviewItem.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './ReviewItem.css';
 
-const ReviewItem = () => {
+const ReviewItem = (props) => {
+    const { name, price, quantity, shipping, img } = props.product;
     return (
-        <div>
-            <h2>This is ReviewItem</h2>
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-items-detail-container">
+                <div className="review-item-detail" title={name}>
+                    <p className="product-name">{name.length > 25 ? name.slice(0, 25) + '...' : name}</p>
+                    <p>Price : <span className='orange-color'>{price}</span></p>
+                    <p><small>Shipping :{shipping}</small></p>
+                    <p><small>Quantity :{quantity}</small></p>
+                </div>
+                <div className="remove-button">
+                    <button>Delete</button>
+                </div>
+            </div>
         </div>
     );
 };
