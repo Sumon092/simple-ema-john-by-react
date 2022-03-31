d341ad5223d418f03286f9968f86fabdcf6b0e28
sumon092 <m.sumon@outlook.com>
2022-03-31 23:47:07 +0600

    orders added

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products)
     return (
         <div>
             <h1>this is orders: {products.length}</h1>
+            <p>Cart has:{cart.length}</p>
         </div>
     );
 };
