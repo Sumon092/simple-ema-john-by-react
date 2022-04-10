cabe655e9a9050bd103bbd7158261f132a275ef3
sumon092 <m.sumon@outlook.com>
2022-04-10 23:17:21 +0600

    implemented login and redirected to shop

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { getAuth } from 'firebase/auth';
+import auth from '../../firebase.init'
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate()
+
+    const [createUserWithEmailAndPassword, hookerror, user] = useCreateUserWithEmailAndPassword(auth)
 
     const handleEmail = (event) => {
         setEmail(event.target.value);
@@ -16,6 +22,9 @@ const SignUp = () => {
     const handleConfirmPassword = (event) => {
         setConfirmPassword(event.target.value);
     }
+    if (user) {
+        navigate('/shop')
+    }
     const handleError = (event) => {
         setError(event.target.value)
     }
@@ -24,6 +33,15 @@ const SignUp = () => {
         if (password !== confirmPassword) {
             setError("Password did't match")
         }
+        if (password.length < 6) {
+            setError('Password Must be at least six characters');
+            return
+        }
+        createUserWithEmailAndPassword(email, password)
+            .then(result => {
+                // const user = result.user;
+                console.log('user created');
+            })
     }
 
 
@@ -43,11 +61,11 @@ const SignUp = () => {
                         </div>
                         <div className="input-group">
                             <label htmlFor="password">Confirm Password</label>
-                            <input onBlur={handleConfirmPassword} type="confirm-password" name="password" id="" placeholder='Confirm your password' />
+                            <input onBlur={handleConfirmPassword} type="password" name="password" id="" placeholder='Confirm your password' />
                         </div>
-                        <p style={{ color: 'red' }} > {error}</p>
+                        <p style={{ color: 'red' }} > {error || hookerror}</p>
                         <div >
-                            <input className="form-submit" type="submit" value="Login" />
+                            <input className="form-submit" type="submit" value="Sign Up" />
                         </div>
                     </form>
                     <p>Already have an account? <Link className='form-link' to='/login'>Login</Link></p>
